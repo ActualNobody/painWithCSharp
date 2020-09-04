4c981157c5231859c7632032b8f7ee82eb30c06d
Kyle King <t0a5ter.m4n1ac@gmail.com>
2020-09-04 14:52:44 +0000

    1 extra thing in BeginnerBasics (filled in parameters)

--- BeginnerBasics.cs
+++ BeginnerBasics.cs
@@ -2,9 +2,9 @@ using System;
 ///*
 class Beginner{
   public void testyThing(string thingy)){
-    Console.WriteLine($"Well, {thingy}?\nIt seems there are {thingy.Length} characters; well that's {thingy.Remove(1)}.\nNow get A LOAD OF THIS!\n{}");
+    Console.WriteLine($"Well, {thingy}?\nIt seems there are {thingy.Length} characters; well that's {thingy.Remove(1)}.\nNow get A LOAD OF THIS!\n{ReverseMe(thingy)}");
   }
-  private string ReverseMe(){
+  private string ReverseMe(string s){
 
   }
 }
