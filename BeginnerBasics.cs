3e5ae29c84c02e168a9a8d249f1934206340e8ce
Kyle King <t0a5ter.m4n1ac@gmail.com>
2020-09-11 14:30:18 +0000

    Put Main in BeginnerBasics

--- BeginnerBasics.cs
+++ BeginnerBasics.cs
@@ -1,7 +1,20 @@
-// using System;
+///*
+using System;
 ///*
 namespace Egregious{
-  public class Beginner{
+  /*public*/ class MainStrt{
+    /*public*/ static void Main(string[] args){
+      Console.WriteLine("Does this?");
+      ComeOn.Beginner howard = new ComeOn.Beginner();
+      // Beginner howard = new Beginner();
+      howard.testyThing("Hey now");
+      // Beginner.testyThing("Hey now");
+    }
+  }
+}
+
+namespace ComeOn{
+  class Beginner{
     public void testyThing(string thingy){
       Console.WriteLine($"Well, {thingy}?\nIt seems there are {thingy.Length} characters; well that's {thingy.Remove(1)}.\nNow get A LOAD OF THIS!\n{/*ReverseMe(thingy)*/thingy}");
     }
@@ -12,11 +25,3 @@ namespace Egregious{
   }
 }
 //*/
-
-/*
-class Beginner{
-  public static void Main(string[] args){
-    Console.WriteLine("Functioning?");
-  }
-}
-*/
--- main.cs
+++ main.cs
@@ -1,27 +1,9 @@
-///*
 using System;
-///*
-namespace Egregious{
-  /*public*/ class MainStrt{
-    /*public*/ static void Main(string[] args){
-      Console.WriteLine("Does this?");
-      // ComeOn.Beginner howard = new ComeOn.Beginner();
-      ComeOn.Beginner howard = new ComeOn.Beginner();
-      howard.testyThing("Hey now");
-      // Beginner.testyThing("Hey now");
+
+namespace Whatever{
+  class Main{
+    static void Main(string[] args){
+
     }
   }
 }
-
-namespace ComeOn{
-  class Beginner{
-    public void testyThing(string thingy){
-      Console.WriteLine($"Well, {thingy}?\nIt seems there are {thingy.Length} characters; well that's {thingy.Remove(1)}.\nNow get A LOAD OF THIS!\n{/*ReverseMe(thingy)*/thingy}");
-    }
-    private string ReverseMe(string s){
-
-      return "";
-    }
-  }
-}
-//*/
